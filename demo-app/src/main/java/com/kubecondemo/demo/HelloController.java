83fc329ae018f97950623983890f680e050ce756
Mustafa Barış EGE <mustafa.ege@tubitak.gov.tr>
2026-03-15 00:29:42 +0300

    bump versin 0.4

--- demo-app/src/main/java/com/kubecondemo/demo/HelloController.java
+++ demo-app/src/main/java/com/kubecondemo/demo/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     public String index() {
         return "<html style='height: 100%; display: flex; justify-content: center; align-items: center; background-color: #0055A4;'>" +
                "<h1 style='font-size: 5rem; color: white; font-family: sans-serif; text-align: center;'>" +
-               "Hello From KubeCon 2026!<br><span style='font-size: 2rem;'>Version 0.1</span>" +
+               "Hello From KubeCon 2026!<br><span style='font-size: 2rem;'>Version 0.4</span>" +
                "</h1>" +
                "</html>";
     }
